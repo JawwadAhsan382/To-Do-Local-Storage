3736fd2408dcb1e41809b0026e0eed5114b0d5e9
JawwadAhsan382 <jawwad654321@gmail.com>
2025-03-13 20:28:13 +0500

    To Do Completed

--- app.js
+++ app.js
@@ -20,32 +20,52 @@ function add(){
     }
     else
     {
-        if(document.getElementById('ulist')==null){
-            getList=document.createElement('ul')
-            getList.setAttribute('id','ulist')
-            document.body.appendChild(getList)
+        let fineString=getInp.value.toLowerCase().split(' ').filter(cv=>cv).join(' ')
+        let n=arr.filter((cv)=>{
+            return cv.ab.toLowerCase().split(' ').filter(cv=>cv).join(' ')==fineString
+        })
+        if (n.length){
+            alert('Item exist')
+            getInp.value=''
         }
-        arr.push({ab:getInp.value})
-        localStorage.setItem('item',JSON.stringify(arr))
-        let textNode=document.createTextNode(getInp.value)
-    let createLi=document.createElement('li')
-    createLi.appendChild(textNode)
-    createLi.innerHTML+=` <button onclick="del(this.parentNode)">Delete</button> <button onclick="edit(this.parentNode)">Edit</button>`
-    getList.appendChild(createLi)
-    getInp.value=''
+        else{
+            if(document.getElementById('ulist')==null){
+                getList=document.createElement('ul')
+                getList.setAttribute('id','ulist')
+                document.body.appendChild(getList)
+            }
+            arr.push({ab:getInp.value})
+            localStorage.setItem('item',JSON.stringify(arr))
+            let textNode=document.createTextNode(getInp.value)
+        let createLi=document.createElement('li')
+        createLi.appendChild(textNode)
+        createLi.innerHTML+=` <button onclick="del(this.parentNode)">Delete</button> <button onclick="edit(this.parentNode)">Edit</button>`
+        getList.appendChild(createLi)
+        getInp.value=''
+        }
     }
 }
 function edit(a){
     let ed=prompt('Enter updated value')
     if (ed){
-        for(i=0;i<arr.length;i++){
-            if((arr[i].ab+' ')==a.firstChild.textContent || (arr[i].ab)==a.firstChild.textContent){
-                arr[i].ab=ed
-                break
-            }
+    let fineString=ed.toLowerCase().split(' ').filter(cv=>cv).join(' ')
+        let n=arr.filter((cv)=>{
+            return cv.ab.toLowerCase().split(' ').filter(cv=>cv).join(' ')==fineString
+        })
+        if (n.length){
+            alert('Item exist')
+            getInp.value=''
         }
-        localStorage.setItem('item',JSON.stringify(arr))
-        a.firstChild.textContent=ed+' '
+        else{
+            for(i=0;i<arr.length;i++){
+                        if((arr[i].ab+' ')==a.firstChild.textContent || (arr[i].ab)==a.firstChild.textContent){
+                            arr[i].ab=ed
+                            break
+                        }
+                    }
+                    localStorage.setItem('item',JSON.stringify(arr))
+                    a.firstChild.textContent=ed+' '
+        }
     }
     else{
         alert('Empty strings are not allowed')
