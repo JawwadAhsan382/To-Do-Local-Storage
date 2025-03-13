c76546729d72f3f36452aa1b604dc425fa992094
JawwadAhsan382 <jawwad654321@gmail.com>
2025-03-13 05:55:32 +0500

    initial

---
+++ app.js
@@ -0,0 +1,73 @@
+let getInp=document.getElementById('inp')
+let getList=document.getElementById('ulist')
+let arr
+if(localStorage.getItem('item')==null){
+    arr=[]
+}
+else{
+    arr=JSON.parse(localStorage.getItem('item'))
+    for(i=0;i<arr.length;i++){
+        let textNode=document.createTextNode(arr[i].ab)
+    let createLi=document.createElement('li')
+    createLi.appendChild(textNode)
+    createLi.innerHTML+=` <button onclick="del(this.parentNode)">Delete</button> <button onclick="edit(this.parentNode)">Edit</button>`
+    getList.appendChild(createLi)
+    }
+}
+function del(a){
+    // for(i=0;i<arr.length;i++){
+    //     if(a.firstChild.textContent==(arr[i].ab+' ')){
+    //         arr.splice(i,1)
+    //         break
+    //     }
+    // }
+    arr=arr.filter((cv)=>{
+        let b=cv.ab
+        b=b+' '
+        return !(b==a.firstChild.textContent)
+    })
+    localStorage.setItem('item',JSON.stringify(arr))
+    a.remove()
+}
+function add(){
+    if(getInp.value==''){
+        alert('Empty strings not allowed')
+    }
+    else
+    {
+        if(document.getElementById('ulist')==null){
+            getList=document.createElement('ul')
+            getList.setAttribute('id','ulist')
+            document.body.appendChild(getList)
+        }
+        arr.push({ab:getInp.value})
+        localStorage.setItem('item',JSON.stringify(arr))
+        let textNode=document.createTextNode(getInp.value)
+    let createLi=document.createElement('li')
+    createLi.appendChild(textNode)
+    createLi.innerHTML+=` <button onclick="del(this.parentNode)">Delete</button> <button onclick="edit(this.parentNode)">Edit</button>`
+    getList.appendChild(createLi)
+    getInp.value=''
+    }
+}
+function edit(a){
+    let ed=prompt('Enter updated value')
+    if (ed){
+        for(i=0;i<arr.length;i++){
+            if((arr[i].ab+' ')==a.firstChild.textContent){
+                arr[i].ab=ed
+                break
+            }
+        }
+        localStorage.setItem('item',JSON.stringify(arr))
+        a.firstChild.textContent=ed
+    }
+    else{
+        alert('Empty strings are not allowed')
+    }
+}
+function delAll(){
+    getList.remove()
+    arr=[]
+    localStorage.removeItem('item')
+}
